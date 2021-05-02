cb17af27834a1c7a97d872291a66253e2479bb3d
Alexandre Trieste <68622225+alexandretrieste@users.noreply.github.com>
2021-05-02 00:40:17 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,6 +1,6 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://susario:senha@cluster-dio.ikgfs.mongodb.net/projeto?retryWrites=true&w=majority',
+		'mongodb+srv://usuario:senha@cluster-dio.ikgfs.mongodb.net/projeto?retryWrites=true&w=majority',
 }
 
