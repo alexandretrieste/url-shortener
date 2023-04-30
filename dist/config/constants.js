26d3e058c72181f4e03edb1f66975260c47bae98
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-04-30 17:53:36 -0300

    chore: added .gitignore, removed unnecessary things

--- dist/config/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://xande013:hugfos-zyvdoj-3bohMe@url-shortener-dio.ikgfs.mongodb.net/urlshortenerdio?retryWrites=true&w=majority',
-};
-//# sourceMappingURL=Constants.js.map
